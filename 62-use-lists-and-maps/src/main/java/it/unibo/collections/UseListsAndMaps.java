704145b85c18102e7ad087b235b6a1ef69062ec4
marco zignani <marco.zignani@studio.unibo.it>
2023-12-02 11:19:13 +0100

    a capo riga 42 esperimento checkout

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -39,6 +39,7 @@ public final class UseListsAndMaps {
          * from 1000 (included) to 2000 (excluded).
          */
         List<Integer> al= new ArrayList<>();
+        
         for (int i = 1000; i < 2000; i++) {
             al.add(i);
         }
